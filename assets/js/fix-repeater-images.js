f926c86c114c0491fbaede225109c664ccc63378
MGTolentino <mgtolentino97@gmail.com>
2025-06-04 19:43:30 -0600

    corregir error de extras select

--- assets/js/fix-repeater-images.js
+++ assets/js/fix-repeater-images.js
@@ -15,6 +15,28 @@
             var randomId = Math.random().toString(36).slice(2);
 
             if (randomId) {
+                // Primero limpiar los elementos de select2 para evitar duplicados
+                $newRow.find('.select2-container').remove();
+                
+                // Limpiar los atributos de select2 de los selects
+                $newRow.find('select[data-select2-id]').each(function() {
+                    var $select = $(this);
+                    // Quitar todos los atributos relacionados con select2
+                    var attrsToRemove = [];
+                    $.each(this.attributes, function(i, attr) {
+                        if (attr.name.indexOf('data-select2') === 0) {
+                            attrsToRemove.push(attr.name);
+                        }
+                    });
+                    // Eliminar los atributos marcados
+                    $.each(attrsToRemove, function(i, attr) {
+                        $select.removeAttr(attr);
+                    });
+                    
+                    // Restablecer el estado del select
+                    $select.removeClass('select2-hidden-accessible');
+                });
+                
                 // Limpiar todos los campos incluyendo los de imágenes
                 $newRow.find(':input').each(function () {
                     var $input = $(this);
@@ -34,27 +56,57 @@
                         } else {
                             $input.val('');
                         }
+                        
+                        // Actualizar IDs para campos de selección
+                        if ($input.is('select')) {
+                            var oldId = $input.attr('id');
+                            if (oldId) {
+                                var newId = oldId.replace(matches[1], randomId);
+                                $input.attr('id', newId);
+                            }
+                        }
                     }
                 });
 
+                // Limpiar campos de imágenes
                 $newRow.find('.hp-field--price-extras-upload, .hp-field--multiple-file').each(function () {
                     var $field = $(this);
 
                     $field.find('.hp-field__previews').empty();
-
                     $field.find('input[type="hidden"].hp-field__value').val('');
-
                     $field.find('.hp-field__counter').text('(0/' + $field.find('.hp-field__file').data('max-files') + ')');
-
                     $field.find('.hp-field__upload-button').removeClass('disabled');
                 });
 
+                // Añadir la nueva fila al DOM antes de inicializar
                 $newRow.appendTo($tbody);
 
-                // Inicializar UI en la nueva fila
+                // Inicializar UI en la nueva fila (incluyendo los select2)
                 if (typeof hivepress !== 'undefined' && hivepress.initUI) {
                     hivepress.initUI($newRow);
                 }
+
+                // Asegurarse de que los select2 se inicialicen correctamente
+                $newRow.find('select[data-component="select"]').each(function() {
+                    var $select = $(this);
+                    
+                    // Si select2 ya está inicializado en este elemento, destruirlo y reinicializarlo
+                    if ($select.data('select2')) {
+                        $select.select2('destroy');
+                    }
+                    
+                    if (typeof $.fn.select2 === 'function') {
+                        // Opciones básicas para select2
+                        var options = {
+                            width: '100%',
+                            minimumResultsForSearch: 20,
+                            dropdownAutoWidth: false
+                        };
+                        
+                        // Inicializar select2
+                        $select.select2(options);
+                    }
+                });
 
                 e.stopPropagation();
                 return false;
